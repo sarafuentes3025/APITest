06c639cea94120ffce2a0fc1f679f9add847add5
Sara Fuentes <sara.fuentes3025@gmail.com>
2021-12-22 16:25:14 -0600

    Add ExtentReport

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = "Steps",
+    plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","json:target/cucumber-reports.json"},
     tags = "@API"
 )
 public class Runner {
